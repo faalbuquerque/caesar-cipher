c1d2297a0118d4f2c04e90c9af1f71708c849a00
Fabiane Albuquerque <bii.nett@gmail.com>
2019-01-31 12:34:00 -0200

    Criei os testes unitarios, alterei css e comecei a criar o ultimo botao

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,9 @@
 //window.cipher = { }; - professor pediu para ignorar por enquanto.
+
 function numeroTexto(){
     let numberCode= parseInt(document.getElementById("number-code1").value);
     let word= document.getElementById("insert-word1").value;
-    return document.getElementById("insert-word1").value = encodeText(numberCode, word);
+    return document.getElementById("insert-word1").value= encodeText(numberCode, word);
   }
 
 function encodeText(numberCode, word){
@@ -13,9 +14,9 @@ function encodeText(numberCode, word){
   let codUltimaMaiuscula= 90;
   let cod1Minuscula= 97;
   let codUltimaMinuscula= 122;
-  let espaco = 32;
+  let espaco= 32;
   
-  for( c= 0; c < word.length; c++ ){
+  for(c= 0; c < word.length; c++){
     codigoDaLetraASC= word[c].charCodeAt();
 
     if(codigoDaLetraASC >= cod1Maiuscula && codigoDaLetraASC <= codUltimaMaiuscula){
@@ -29,22 +30,15 @@ function encodeText(numberCode, word){
     if(codigoDaLetraASC === espaco){
       letras= letras + (String.fromCharCode(codigoDaLetraASC));
     } 
+    
   }
   return letras;
 }
 
-/*
-var teste = encodeText(1,"abcd");
-if(teste === "bcde"){
-console.log("Funciona!")
-} else{
-console.log("Nao funciona!")
-}*/
-
 function decodeNumeroTexto(){
   let desloc= parseInt(document.getElementById("number-code2").value);
   let palavra= document.getElementById("insert-word2").value;
-  return document.getElementById("insert-word2").value = decodeText(desloc, palavra);
+  return document.getElementById("insert-word2").value= decodeText(desloc, palavra);
 }
 
 function decodeText(desloc, palavra){
@@ -55,17 +49,17 @@ function decodeText(desloc, palavra){
   let codUltimaMaiuscula= 90;
   let cod1Minuscula= 97;
   let codUltimaMinuscula= 122;
-  let espaco = 32;
+  let espaco= 32;
  
   for( c= 0; c < palavra.length; c++ ){
     codigoDaLetraASC= palavra.charCodeAt(c);
 
     if(codigoDaLetraASC >= cod1Maiuscula && codigoDaLetraASC <= codUltimaMaiuscula){
-      letras= letras + (String.fromCharCode(((codigoDaLetraASC - cod1Maiuscula - (desloc % tamDoAlfabeto) + tamDoAlfabeto) % tamDoAlfabeto +cod1Maiuscula)));
+      letras= letras + (String.fromCharCode(((codigoDaLetraASC - cod1Maiuscula - (desloc % tamDoAlfabeto) + tamDoAlfabeto) % tamDoAlfabeto + cod1Maiuscula)));
     } 
     
     if(codigoDaLetraASC >= cod1Minuscula && codigoDaLetraASC <= codUltimaMinuscula){
-      letras= letras + (String.fromCharCode(((codigoDaLetraASC - cod1Minuscula - ( desloc % tamDoAlfabeto) + tamDoAlfabeto)%tamDoAlfabeto + cod1Minuscula)));
+      letras= letras + (String.fromCharCode(((codigoDaLetraASC - cod1Minuscula - (desloc % tamDoAlfabeto) + tamDoAlfabeto)%tamDoAlfabeto + cod1Minuscula)));
     } 
 
     if(codigoDaLetraASC === espaco){
@@ -73,4 +67,10 @@ function decodeText(desloc, palavra){
     }
   }
   return letras;
+}
+
+
+
+function voltarTela() {
+  document.getElementById("voltar-tela").style.display = "none";
 }
---
+++ src/tests.js
@@ -0,0 +1,66 @@
+  
+  function test(resultFuncao, resultCorreto){
+
+    if(resultFuncao === resultCorreto){
+        console.log("Funciona!")
+    }else{
+        console.log("Nao funciona!")
+        }
+    }
+
+
+console.log("Teste de codificacao de letras Minusculas:");
+test(encodeText(1, "abcd") , "bcde");
+
+console.log("Teste de codificacao de letras Maiusculas:");
+test(encodeText(10, "ABCD"), "KLMN");
+
+console.log("Teste de codificacao de letras minusculas com espaco:");
+test(encodeText(20, "w x y z"), "q r s t");
+
+console.log("Teste de codificacao de letras maiusculas com espaco:");
+test(encodeText(27, "A B C D"), "B C D E");
+
+console.log("Teste de codificacao de letras maiusculas com deslocamento alto:");
+test(encodeText(50, "FGHI"), "DEFG");
+
+console.log("Teste de codificacao de frases com letras maiusculas, minusculas e espaco:");
+test(encodeText(5, "Ola Fabiane"), "Tqf Kfgnfsj");
+
+console.log("Teste de codificacao de frase grande:");
+test(encodeText(3, "Era um biquini de bolinha amarelinha tao pequenininho Mal cabia na Ana Maria Biquini de bolinha amarelinha tao pequenininho Que na palma da mao se escondia"), "Hud xp eltxlql gh erolqkd dpduholqkd wdr shtxhqlqlqkr Pdo fdeld qd Dqd Pduld Eltxlql gh erolqkd dpduholqkd wdr shtxhqlqlqkr Txh qd sdopd gd pdr vh hvfrqgld");
+
+console.log("Teste de codificacao de frase com muito espacamento:");
+test(encodeText(40, "Com      MUITO      espaco"), "Qca      AIWHC      sgdoqc");
+
+
+console.log("Teste de decodificacao de letras minusculas:");
+test(decodeText(1, "bcde"), "abcd");
+
+console.log("Teste de decodificacao de letras maiusculas:");
+test(decodeText(10, "KLMN"), "ABCD");
+
+console.log("Teste de decodificacao de letras minusculas com espaco:");
+test(decodeText(20, "q r s t"), "w x y z");
+
+console.log("Teste de decodificacao de letras maiusculas com espaco:");
+test(decodeText(27, "B C D E"), "A B C D");
+
+console.log("Teste de decodificacao de letras maiusculas com deslocamento alto:");
+test(decodeText(50, "DEFG"), "FGHI");
+
+console.log("Teste de decodificacao de frases com letras maiusculas, minusculas e espaco:");
+test(decodeText(5, "Tqf Kfgnfsj"), "Ola Fabiane");
+
+console.log("Teste de decodificacao de de frase grande:");
+test(decodeText(3, "Hud xp eltxlql gh erolqkd dpduholqkd wdr shtxhqlqlqkr Pdo fdeld qd Dqd Pduld Eltxlql gh erolqkd dpduholqkd wdr shtxhqlqlqkr Txh qd sdopd gd pdr vh hvfrqgld"), "Era um biquini de bolinha amarelinha tao pequenininho Mal cabia na Ana Maria Biquini de bolinha amarelinha tao pequenininho Que na palma da mao se escondia");
+
+console.log("Teste de decodificacao de frase com muito espacamento:");
+test(decodeText(40, "Qca      AIWHC      sgdoqc"), "Com      MUITO      espaco");
+
+
+
+
+
+
+
