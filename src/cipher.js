f9874d3342ac6695a4bfaa954e0a49094e28125f
Fabiane Albuquerque <bii.nett@gmail.com>
2019-02-02 00:37:01 -0200

    Ultimas alteracoes

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,36 @@
-//window.cipher = { }; - professor pediu para ignorar por enquanto.
 
- function choicecode(code){
+function botaoVoltar() {
+  document.getElementById("text-encoder").style.display = "block";
+  document.getElementById("number-code1").value = "";
+  document.getElementById("insert-word1").value = "";
+  document.getElementById("text-decoder").style.display = "block";
+  document.getElementById("number-code2").value = "";
+  document.getElementById("insert-word2").value = "";
+  document.getElementById("decode").style.display = "block";
+  document.getElementById("code").style.display = "block";
+  document.getElementById("middle").style.display = "none";
+}
 
- }
-
-
- function choicedecode(){
+function choiceCode(){
+  document.getElementById("text-decoder").style.display = "none";
+  document.getElementById("decode").style.display = "none";
+  document.getElementById("middle").style.display = "block";
   
- }
+}
 
+function choiceDecode(){
+  document.getElementById("text-encoder").style.display = "none";
+  document.getElementById("code").style.display = "none";
+  document.getElementById("middle").style.display = "block";
+}
+ 
 function numeroTexto(){
-    let numberCode= parseInt(document.getElementById("number-code1").value);
-    let word= document.getElementById("insert-word1").value;
-    return document.getElementById("insert-word1").value= encodeText(numberCode, word);
-  }
-function encodeText(numberCode, word){
+  let numberCode= parseInt(document.getElementById("number-code1").value);
+  let word= document.getElementById("insert-word1").value;
+  return document.getElementById("insert-word1").value= encodeText(numberCode, word);
+}
 
+function encodeText(numberCode, word){
   let letras= "";
   let codigoDaLetraASC= [];
   const tamDoAlfabeto= 26;
@@ -44,8 +59,8 @@ function decodeNumeroTexto(){
   let palavra= document.getElementById("insert-word2").value;
   return document.getElementById("insert-word2").value= decodeText(desloc, palavra);
 }
-function decodeText(desloc, palavra){
 
+function decodeText(desloc, palavra){
   let letras= "";
   let codigoDaLetraASC= [];
   const tamDoAlfabeto= 26;
@@ -66,10 +81,3 @@ function decodeText(desloc, palavra){
   }
   return letras;
 }
-
-
-
-function botaoVoltar() {
-  document.getElementById("voltar-tela").style.display = "none";
-}
-
